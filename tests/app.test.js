497821c4e002acb490eaa9ddbb23f94b4b51c01a
kabir1009 <babarindeabdulkabiradio.com>
2026-06-10 13:30:43 +0100

    update

--- tests/app.test.js
+++ tests/app.test.js
@@ -2,19 +2,16 @@ const request = require("supertest");
 const app = require("../app");
 
 test("GET / returns application details", async () => {
-
-  const response =
-    await request(app).get("/");
+  const response = await request(app).get("/");
 
   expect(response.statusCode).toBe(200);
-	test("GET /health returns healthy", async () => {
-
-  const response =
-    await request(app).get("/health");
-
-  expect(response.statusCode).toBe(200);
-
-  expect(response.body.status)
-    .toBe("healthy");
-
 });
+
+test("GET /health returns healthy", async () => {
+  const response = await request(app).get("/health");
+
+  expect(response.statusCode).toBe(200);
+
+  expect(response.body.status).toBe("healthy");
+});
+
