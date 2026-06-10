f468a35ab1a9b6bbd3d5d22f4d1bbabba1eea5dd
kabir1009 <babarindeabdulkabiradio.com>
2026-06-10 12:57:42 +0100

    update

--- tests/app.test.js
+++ tests/app.test.js
@@ -7,5 +7,14 @@ test("GET / returns application details", async () => {
     await request(app).get("/");
 
   expect(response.statusCode).toBe(200);
+	test("GET /health returns healthy", async () => {
+
+  const response =
+    await request(app).get("/health");
+
+  expect(response.statusCode).toBe(200);
+
+  expect(response.body.status)
+    .toBe("healthy");
 
 });
